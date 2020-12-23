48456176853c42286bf9b2e677c0e7cb964f716a
nigelreign <zulunigelb@gmail.com>
2020-12-23 12:25:47 +0200

    working with lists and conditionals

--- src/App.js
+++ src/App.js
@@ -1,40 +1,63 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const [ personState, setPersonState ] = useState(
-    {
-      persons: [
-        { name: "Nigel", age: 26},
-        { name: "Natalie", age: 23},
-        { name: "Daniel", age: 24}
-      ],
-      otherState: 'some state'
-    }
-  );
-  const switchNameHandler = (newName) =>{
-    setPersonState({
+class App extends Component {
+  state = {
+    persons: [
+        { id: '1', name: "Nigel", age: 26},
+        { id: '2', name: "Natalie", age: 23},
+        { id: '3', name: "Daniel", age: 24}
+    ],
+    otherState: 'Some other state',
+    showPersons: false
+  }
+
+  switchNameHandler = (newName) => {
+    this.state({
           persons: [
       { name: newName, age: 26},
       { name: "Natalie", age: 23},
       { name: "Lungani", age: 22}
     ],
-    otherState: personState.otherState
-    });
+    })
   }
 
 // Event handler from an input passed to a componet using props
-  const changeNameHandler = (event) =>{
-    setPersonState({
-          persons: [
-      { name: "Nigel", age: 26},
-      { name: event.target.value, age: 23},
-      { name: "Lungani", age: 22}
-    ],
-    otherState: personState.otherState
+  changeNameHandler = (event, id) =>{
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+// It is good practice to not mutate the array directly 
+// thats why we are creating a new object
+    const person = { ...this.state.persons[personIndex]};
+
+// assign the new name typed in the input box to the name in the array
+    person.name = event.target.value;
+
+// call the persons array
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons})
   }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // This will create a copy of the array before splicing it
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    //This removes one element from the array
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  }
+
+  render (){
 
   // Inline css styling
   const style = {
@@ -44,29 +67,39 @@ const App = props => {
     padding: '8px',
     cursor: 'pointer'
   }
+
+  let persons = null;
+  if(this.state.showPersons){
+    persons = (
+      <div>
+        {/* a loop */}
+        { this.state.persons.map((person, index ) => {
+          return <Person 
+            click = {()=> this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age}
+            // the key property allows react to keep track of changes
+            key={person.id}
+            changed = { (event) => this.changeNameHandler (event, person.id) }
+          />
+        }) }
+    </div>
+    );
+  }
   return (
     // adding external css style sheet
     <div className="App">
-      <h1>Hello World</h1>
-      <p>this is a paragraph</p>
+      <h1>This is my react app</h1>
       {/* using the arrow function to pass data to a function... Not recommended */}
       <button 
         style = {style}
-        onClick={() => switchNameHandler('Mbongeni Nkiwane')}>
-          Switch Name
+        onClick={this.togglePersonsHandler}>
+          Show Cards
       </button>
-      <Person name={ personState.persons[0].name }  age={ personState.persons[0].age }/>
-      {/* passing functions as a prop */}
-      {/* using the bind method to pass data to a function... Recommended */}
-      <Person 
-        click={ switchNameHandler.bind(this, 'Jogn Cena') }
-        changed = {changeNameHandler}
-        name={ personState.persons[1].name } age={ personState.persons[1].age }>
-           I love cooking 
-      </Person>
-      <Person name={ personState.persons[2].name } age={ personState.persons[2].age }/>
+      { persons }
     </div>
   );
   }
+}
 
 export default App;
