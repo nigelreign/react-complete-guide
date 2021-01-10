919adbd1f107c1513eb01150bd954be8d593a754
nigelreign <zulunigelb@gmail.com>
2021-01-10 23:14:38 +0200

    rendering adjacent jsx elements

---
+++ src/Hoc/Aux.js
@@ -0,0 +1,7 @@
+// It helps create and render html tags in JSX without wrapping them with a div tag
+
+import React from 'react';
+
+const aux = props => props.children;
+
+export default aux;
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 // exporting external css style sheet
 import './Person.css';
+import Aux from '../../../Hoc/Aux'
 
 class Person extends Component{
     render (){
         console.log("[Person.js] renderrinnggg....");
     return (
-        // calling a css class name
-        <div className='Person'>
-            <p onClick={ this.props.click }>My name is {this.props.name} and i am { this.props.age } years old</p>
-            <p>{this.props.children}</p>
-            <input type='text' onChange={ this.props.changed } value={this.props.name} />
-        </div>
+        <Aux>
+            {/* // calling a css class name */}
+            <div className='Person'>
+                <p onClick={ this.props.click }>My name is {this.props.name} and i am { this.props.age } years old</p>
+                <p>{this.props.children}</p>
+                <input type='text' onChange={ this.props.changed } value={this.props.name} />
+            </div>
+        </Aux>
      )
     }
 };
