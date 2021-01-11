f8c152605bfc40773251fd94a955fc104c533165
nigelreign <zulunigelb@gmail.com>
2021-01-11 22:01:20 +0200

    using refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,31 +7,41 @@ import './Person.css';
 import Aux from '../../../Hoc/Aux'
 
 class Person extends Component{
+    // This will set focus to the last element of input
+    componentDidMount(){
+        this.inputElement.focus();
+    };
+
     render (){
         console.log("[Person.js] renderrinnggg....");
-    return (
-        // React also has a built in Aux component called React.Fragment you can use it using any method below
-        /* import React, { Fragment } from 'react'
+        return (
+            // React also has a built in Aux component called React.Fragment you can use it using any method below
+            /* import React, { Fragment } from 'react'
 
-            <Fragment>
-                some JSX code
-            </Fragment>
+                <Fragment>
+                    some JSX code
+                </Fragment>
 
-            OR
-            <React.Fragment>
-                some JSX code
-            </React.Fragment>
-         */
-        <Aux>
-            {/* // calling a css class name */}
-            <div className='Person'>
-                <p onClick={ this.props.click }>My name is {this.props.name} and i am { this.props.age } years old</p>
-                <p>{this.props.children}</p>
-                <input type='text' onChange={ this.props.changed } value={this.props.name} />
-            </div>
-        </Aux>
-     )
-    }
+                OR
+                <React.Fragment>
+                    some JSX code
+                </React.Fragment>
+            */
+            <Aux>
+                {/* // calling a css class name */}
+                <div className='Person'>
+                    <p onClick={ this.props.click }>My name is {this.props.name} and i am { this.props.age } years old</p>
+                    <p>{this.props.children}</p>
+                    <input
+                    ref={(inputEl) => {this.inputElement = inputEl} }
+                    type='text'
+                    onChange={ this.props.changed }
+                    value={this.props.name}
+                    />
+                </div>
+            </Aux>
+        )
+        }
 };
 
 Person.propTypes = {
