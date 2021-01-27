8b1823276c27c783cb89b4c04e1d358bea1611d0
nigelreign <zulunigelb@gmail.com>
2021-01-27 23:21:18 +0200

    New Project: The burger builder

--- src/Hoc/Aux.js
+++
@@ -1,7 +0,0 @@
-// hoc stands for Higher Order Components
-
-// It helps create and render html tags in JSX without wrapping them with a div tag
-
-const aux = props => props.children;
-
-export default aux;
